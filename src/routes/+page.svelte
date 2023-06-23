900c57b4c4c7c0f42cdd9a20770d8ef9af8a42c5
coopTRUE <coopdog4545@gmail.com>
2023-06-23 11:54:31 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Home | Cooper</title>
+  <meta name="description" content="" />
+</svelte:head>
+
+<main>
+  <h1>hi</h1>
+</main>
+
+<style lang="scss">
+  h1 {
+    color: green;
+  }
+</style>
