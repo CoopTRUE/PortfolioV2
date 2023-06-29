3b3218381c589d9169d27800fd18812aa5e505ff
coopTRUE <coopdog4545@gmail.com>
2023-06-28 23:59:49 -0400

    starting work on nav

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  const links = {
+    '/': 'home',
+    '/works': 'works',
+    '/about-me': 'about-me',
+    '/contacts': 'contacts',
+  }
+  $: ({ pathname } = $page.url)
+</script>
+
+<header>
+  <nav>
+    <div class="logo">
+      <p>Cooper</p>
+    </div>
+    <ul class="links">
+      {#each Object.entries(links) as [link, name]}
+        {@const active = pathname === link}
+        <li class="link" class:active>
+          <a href={link}>{name}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+</header>
+
+<style lang="scss">
+  nav {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+  }
+  .links {
+    display: flex;
+    gap: 2rem;
+  }
+  .link {
+    &.active {
+      a {
+        color: white;
+      }
+    }
+    a {
+      color: var(--color-gray);
+      font-weight: 500;
+      text-decoration: none;
+      transition: color 0.2s ease-in-out;
+      &:hover {
+        color: white;
+      }
+    }
+    &::before {
+      content: '#';
+      color: var(--color-primary);
+    }
+  }
+  .logo {
+    color: white;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
+  import Header from '$lib/components/Header.svelte'
   import '../global.scss'
 </script>
 
+<Header />
 <slot />
