9688dea970518069e9400349e0388a6857976305
coopTRUE <coopdog4545@gmail.com>
2023-07-02 14:21:54 -0400

    Made the projects page have real projects

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,7 +1,8 @@
 import alphag3n from '$lib/images/projects/alphag3n.png?w=1500&format=webp'
 import grapeCasino from '$lib/images/projects/grape-casino.png?w=1500&format=webp'
-import maven from '$lib/images/projects/maven.png?w=1500&format=webp'
-import mvCasino from '$lib/images/projects/mv-casino.png?w=1500&format=webp'
+
+// import maven from '$lib/images/projects/maven.png?w=1500&format=webp'
+// import mvCasino from '$lib/images/projects/mv-casino.png?w=1500&format=webp'
 
 interface Project {
   title: string
@@ -14,32 +15,25 @@ interface Project {
 const projects: Project[] = [
   {
     title: 'AlphaG3n',
-    description: 'A website for a music producer',
-    toolsUsed: ['Svelte', 'SvelteKit', 'TypeScript', 'Tailwind CSS'],
+    description: 'A project where highschoolers can learn about crypto and the blockchain',
+    toolsUsed: ['Svelte', 'SvelteKit', 'TypeScript', 'Prisma', 'MySql'],
     link: 'https://alphag3n.com',
     src: alphag3n,
   },
   {
     title: 'Grape Casino',
-    description: 'A website for a casino',
+    description: "Grape Finance's centralized casino running on the fantom blockchain",
     toolsUsed: ['Svelte', 'SvelteKit', 'TypeScript'],
     link: 'https://grape-casino.com',
     src: grapeCasino,
   },
-  {
-    title: 'Maven',
-    description: 'A website for a music produceasdfasdjfhjkasdfkjasr',
-    toolsUsed: ['Svelte', 'SvelteKit', 'TypeScript'],
-    link: 'https://mavenmusic.net',
-    src: maven,
-  },
-  {
-    title: 'MV Casino',
-    description: 'A website for a casino',
-    toolsUsed: ['Svelte', 'SvelteKit', 'TypeScript'],
-    link: 'https://mv-casino.com',
-    src: mvCasino,
-  },
+  // {
+  //   title: 'MV Casino',
+  //   description: 'A website for a casino',
+  //   toolsUsed: ['Svelte', 'SvelteKit', 'TypeScript'],
+  //   link: 'https://mv-casino.com',
+  //   src: mvCasino,
+  // },
 ]
 
 export default projects
